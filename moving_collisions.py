722297d97c05530d496ffdf906ebd328f2331bce
253756 <m253756@usna.edu>
2022-12-05 02:10:08 -0500

    add two players -- and moving them

--- moving_collisions.py
+++ moving_collisions.py
@@ -15,9 +15,10 @@ screen.fill((0,0,0))
 screen_rect = screen.get_rect()
 rows = screen_rect.height // tile_size
 cols = screen_rect.width // tile_size
+clock = pygame.time.Clock()
 
 
-#drawing my ocean onthe screen
+#drawing my ocean on the screen
 def update():
     for x in range(int(rows)):
         for y in range(int(cols)):
@@ -26,12 +27,15 @@ def update():
 #bring in player 1
 player1 = Ship1()
 
-pygame.display.flip()
+#bring in player 2
+player2 = Ship1()
+
 #points for the first ship origin
 x = 320
 y=600
 
-clock = pygame.time.Clock()
+f = 160
+g = 600
 obstacle_speed = 3
 obs_y = -30
 speed = 1
@@ -42,7 +46,7 @@ life = Health(screen)
 
 while True:
     for event in pygame.event.get():
-        if event.type ==pygame.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     keys = pygame.key.get_pressed()
@@ -53,18 +57,32 @@ while True:
     # set the boundaries
     if x >= 624:
         x = 624
-        # need to fix this currerntly center is at the end
     if x < 21:
         x = 20
+    #movement of player 2
+    if keys[pygame.K_a]:
+        f -= 3
+    if keys[pygame.K_d]:
+        f += 3
+    if f >= 624:
+        f = 624
+    if f < 21:
+        f = 20
+
+    #when obstacle leaves the boundaries
     if obs_y >= 640:
         obs_y = -30
         obs_x = random.randint(0,640)
         obstacle_speed = random.randint(2,5)
+    # obstacle move down the screen
     obs_y = obs_y + obstacle_speed
+
     obstacle = Obstacle1(screen, obs_x, obs_y)
     obstacle2 = Obstacle1(screen, 0, 0)
     obstacle_rect = obstacle.rect
+
     collision = pygame.sprite.collide_rect(player1, obstacle)
+    collision = pygame.sprite.collide_rect(player2,obstacle)
     if collision:
         player1.health -= 50
         print("you just got hit")
@@ -72,14 +90,17 @@ while True:
         obs_y = -30
         obs_x = random.randint(0, 640)
         obstacle_speed = random.randint(2, 5)
+
     update()
     player1_rect = player1.rect
     player1.draw(screen)
+    player2.draw(screen)
     obstacle.draw(screen)
-    obstacle2.draw(screen)
-    life.draw(screen)
+    #obstacle2.draw(screen)
+    #life.draw(screen)
     coordinate = (x,y)
+    coordinate2 = (f,g)
+    player2.move(coordinate2)
     player1.move(coordinate)
     clock.tick(100)
     pygame.display.flip()
-
